c53cbf79489deec78671c711b4bfbaeb7dd62f5b
Matthew Edelen <84196639+mteam88@users.noreply.github.com>
2022-09-14 19:23:39 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.16
+
+require (
+	github.com/ethereum/go-ethereum v1.10.24 // indirect
+	github.com/gochain/web3 v0.2.92 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+    "context"
+    "log"
+    "crypto/ecdsa"
+    "math/big"
+    "time"
+    "os"
+
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/core/types"
+    "github.com/ethereum/go-ethereum/crypto"
+    "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/params"
+    "github.com/joho/godotenv"
+)
+
+func etherToWei(val *big.Int) *big.Int {
+	return new(big.Int).Mul(val, big.NewInt(params.Ether))
+}
+
+func weiToEther(val *big.Int) *big.Int {
+	return new(big.Int).Div(val, big.NewInt(params.Ether))
+}
+
+func checkerr(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func main() {
+    godotenv.Load(".envrc")
+    
+    fmt.Println(os.Getenv("NODE_ENDPOINT"))
+    
+    client, err := ethclient.Dial(os.Getenv("NODE_ENDPOINT"))
+    checkerr(err)
+
+    privateKey, err := crypto.HexToECDSA(os.Getenv("TARGET_PRIVATE_KEY"))
+    
+    checkrate := time.Duration(15000000000)
+    for {
+        time.Sleep(checkrate)
+        // Get the balance of an account
+        publicKey := privateKey.Public()
+        publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+        if !ok {
+            log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+        }
+    
+        fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+        nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+        checkerr(err)
+        
+        balance, err := client.BalanceAt(context.Background(), fromAddress, nil)
+        checkerr(err)
+        
+        fmt.Println("Nonce: ", nonce)
+        fmt.Println("Account balance:", balance)
+    
+        
+        gasLimit := uint64(21000)          // in units
+        gasPrice, err := client.SuggestGasPrice(context.Background())
+        checkerr(err)
+        
+        gasLimitBigInt := new(big.Int).SetUint64(gasLimit)
+        gasExpense := new(big.Int)
+        gasExpense.Mul(gasLimitBigInt, gasPrice)
+        fmt.Println("gasExpense: ", gasExpense)
+    
+        value := new(big.Int).Sub(balance, gasExpense)
+        fmt.Println("value: ", value)
+
+        if value.Int64() >= 0 {
+        
+            toAddress := common.HexToAddress("0x446851f793Bc93E666aE89e9aC840DE968308e5d")
+            var data []byte
+            tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+        
+            chainID, err := client.NetworkID(context.Background())
+            checkerr(err)
+        
+            signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+            checkerr(err)
+        
+            err = client.SendTransaction(context.Background(), signedTx)
+            checkerr(err)
+        
+            balance, err = client.BalanceAt(context.Background(), fromAddress, nil)
+            checkerr(err)
+            
+            fmt.Printf("tx sent: %s\n", signedTx.Hash().Hex())
+        
+            txHash := signedTx.Hash()
+            sleeptime := time.Duration(30000000000)
+            for {
+                time.Sleep(sleeptime)
+                _, isPending, err := client.TransactionByHash(context.Background(), txHash)
+                checkerr(err)
+                fmt.Println("isPending: ", isPending)
+                if !isPending {
+                    fmt.Println("TX done.")
+                    break
+                }
+            }
+        }
+    }
+}
